9b67500c28986c3e50a8ea1d38cf120751c0a61e
Moritz Roehrer <moritz.robert.fischer@gmail.com>
2025-08-22 15:35:22 +0200

    Initial commit

---
+++ typr.py
@@ -0,0 +1,31 @@
+import wonderwords
+from wonderwords import RandomWord
+
+text = ""
+
+def toType():
+    print(text)
+
+user_input = ""
+def userInput():
+    global user_input
+    user_input = input(">>>")
+
+def check():
+    if user_input == text:
+        return 0
+    else:
+        print("Incorrect. Try again.")
+        toType()
+        userInput()
+
+def newText():
+    global text
+    text = RandomWord().word()
+
+newText()
+while True:
+    toType()
+    userInput()
+    check()
+    newText()
